7013c2d81256b61c63a0adae2212d0c5f79b94e6
Orji Michael <xlence1624@gmail.com>
2026-09-06 04:17:40 +0100

    qq

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,16 +1,13 @@
 import { v2 as cloudinary } from "cloudinary";
-import dotenv from "dotenv";
 
-dotenv.config();
 
-console.log("Cloudinary cloud name:", process.env.CLOUDINARY_CLOUD_NAME);
-console.log("Cloudinary API key exists:", !!process.env.CLOUDINARY_API_KEY);
-console.log("Cloudinary secret exists:", !!process.env.CLOUDINARY_API_SECRET);
+
+
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name:drrxxbqnz,
+ api_key: 962773679322971,
+  api_secret: dhPiy9HoDy7Uwgl4JT2l2q3zJVI
 });
 
 export default cloudinary;
