72d6de886c3844028e7fce86b437683c5f96b322
Orji Michael <xlence1624@gmail.com>
2026-09-06 10:04:21 +0100

    fixer

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -5,9 +5,9 @@ import { v2 as cloudinary } from "cloudinary";
 
 
 cloudinary.config({
-  cloud_name:drrxxbqnz,
+  cloud_name:"drrxxbqnz",
  api_key: 962773679322971,
-  api_secret: dhPiy9HoDy7Uwgl4JT2l2q3zJVI
+  api_secret: "dhPiy9HoDy7Uwgl4JT2l2q3zJVI"
 });
 
 export default cloudinary;
--- server.js
+++ server.js
@@ -18,28 +18,11 @@ import commentRoutes from "./routes/commentRoute.js";
 // import userRoutes from "./routes/userRoute.js";
 // import commentRoutes from "./routes/commentRoutes.js";
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-await connectDB();
-
-app.use("/api/auth", authRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api", commentRoutes);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-export default app;
-
 // const app = express();
-// const port = 3000;
+
 // app.use(cors());
 // app.use(express.json());
+
 // await connectDB();
 
 // app.use("/api/auth", authRoutes);
@@ -47,12 +30,29 @@ export default app;
 // app.use("/api/users", userRoutes);
 // app.use("/api", commentRoutes);
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
 // });
 
+// export default app;
+
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
+await connectDB();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api", commentRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
